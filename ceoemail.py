ba6689999a068868cf3e738a40dbb34c4de7b970
Jason Goretzki <jason@akroncdnr.com>
2016-08-01 03:06:22 +0000

    Updated proxy logic and method, added port argument

--- ceoemail.py
+++ ceoemail.py
@@ -1,41 +1,69 @@
 #!/usr/bin/env python
+
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.proxy import *
 from HTMLParser import HTMLParser
 from BeautifulSoup import BeautifulSoup as bs
 from pyvirtualdisplay import Display
 
+import time
 import argparse
 
-parser = argparse.ArgumentParser(description='Scrape Factual')
+def ChangeProxy(ProxyHost ,ProxyPort):
+    print "Define Firefox Profile " + ProxyHost + " : " + ProxyPort
+    profile = webdriver.FirefoxProfile()
+    profile.set_preference("network.proxy.type", 1)
+    profile.set_preference("network.proxy.http", ProxyHost )
+    profile.set_preference("network.proxy.http_port", int(ProxyPort))
+    profile.set_preference("general.useragent.override","Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:21.0) Gecko/20100101 Firefox/21.0")
+    profile.update_preferences()
+    return webdriver.Firefox(firefox_profile=profile)
+
+def FixProxy():
+    print "Reset Firefox Profile"
+    profile = webdriver.FirefoxProfile()
+    profile.set_preference("network.proxy.type", 0)
+    return webdriver.Firefox(firefox_profile=profile)
+
+parser = argparse.ArgumentParser(description='Scrape ceoemail')
 parser.add_argument('-p', '--proxy', type=str, required=True)
+parser.add_argument('-t', '--port', type=str, required=True)
 parser.add_argument('-u', '--url', type=str, required=True)
 args = parser.parse_args()
-print args.proxy 
-print args.url
-
-webdriver.DesiredCapabilities.FIREFOX['proxy']={
-	"httpProxy":args.proxy,
-	"ftpProxy":args.proxy,
-	"sslProxy":args.proxy,
-	"noProxy":None,
-	"proxyType":"MANUAL",
-	"autodetect":False
-}
+print "Args loaded"
 
 display = Display(visible=0, size=(800, 600))
 display.start()
-browser = webdriver.Firefox()
+print "Display started"
+browser = ChangeProxy(args.proxy,args.port)
+#browser = webdriver.Firefox()
 browser.get(args.url)
 html = browser.page_source.encode('ascii', 'xmlcharrefreplace')
-soup = bs(html)
-links = browser.find_elements_by_xpath('//a[contains(@href, "%s")]' % 's.php?id=ceo')
-for link in links:
-	print link.get_attribute('href')
-	browser.implicitly_wait(30)
-	link.click()
+exit = 0
+while exit == 0:
+        if "email addresses found" not in html:
+                browser.implicitly_wait(10)
+		print "Waiting..."
+		html = browser.page_source.encode('ascii', 'xmlcharrefreplace')
+	else:
+		exit = 1
+html = browser.page_source.encode('ascii', 'xmlcharrefreplace')
+print html
+counter = 0
+elements = browser.find_elements_by_xpath('//a[contains(@href, "%s")]' % 's.php?id=ceo')
+for counter in range(counter, len(elements)):
+	elements = browser.find_elements_by_xpath('//a[contains(@href, "%s")]' % 's.php?id=ceo')
+	print elements[counter].get_attribute('href')
+	elements[counter].click()
 	source = browser.page_source.encode('ascii', 'xmlcharrefreplace')
-	print source
-	browser.get(args.url)
+	if "if you are using this site in relation to your work or you are seeking lists of email addresses please" not in source:
+		print source
+	if "mailto" in source:
+		print source
+	else:
+		print "Failed ",counter," Proxy: ",args.proxy," URL: ",elements[counter]
+	browser.back()
+	time.sleep(5)
 browser.quit()
--- ceoemail.sh
+++ ceoemail.sh
@@ -12,13 +12,14 @@ do
   # Grab a random proxy
   proxy=`grep -n $ proxy.list | grep $(echo ${RANDOM} | cut -c1) | cut -d: -f2`
   echo "Examaning site ${site}"
-  python ceoemail.py --proxy none-ip.info --url ${site} > output.txt
+  python ceoemail.py --proxy "${proxy}" --url ${site} > output.txt
   links=$(cat output.txt | sed 's/href=\"/\n/g')
   links=$(cat output.txt | grep href=\"s.php output.txt | sed 's/href=\"/\n/g' | cut -d'"' -f1 | grep ceo)
   for link in ${links}
   do
     echo "Scraping ${link}"
-    python ceoemail.py --proxy none-ip.info --url "http://ceoemail.com/${link}" > payload/${loop}.txt
+    proxy=`grep -n $ proxy.list | grep $(echo ${RANDOM} | cut -c1) | cut -d: -f2`
+    python ceoemail.py --proxy "${proxy}" --url "http://ceoemail.com/${link}" > payload/${loop}.txt
     loop=`expr ${loop} + 1`
   done
   sleep 30
